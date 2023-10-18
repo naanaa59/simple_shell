908bc6e301d4bde5c42d86fdeb07436eb0026141
oumaima <oumaimanaanaapro@gmail.com>
2023-10-18 11:21:07 +0100

    add customized functions

--- function_2.c
+++ function_2.c
@@ -39,4 +39,58 @@ int _putchar(int std, char c)
 {
 	return (write(std, &c, 1));
 }
+/**
+ *_isalpha - Returns 1 if c is a letter, lowercase or uppercase 0 otherwise
+ *@c:charachter to test
+ *Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	int r = 0;
 
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		r = 1;
+	else
+		r = 0;
+	return (r);
+}
+/**
+ * interactive_mode - create an interactive mode in sh
+ */
+void interactive_mode(void)
+{
+	int t;
+
+	t = isatty(STDIN_FILENO);
+	if (t == 1)
+	{
+		printf("$ ");
+		fflush(stdout);
+	}
+}
+/**
+ * _strdup - copy of the string given as a parameter.
+ *
+ * @str: the given string to copy
+ *
+ * Return: a pointer to the duplicata
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, j;
+	int size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		size++;
+	s = malloc(sizeof(char) * (size + 1));
+	if (s == NULL)
+		return (NULL);
+	for (j = 0; j < size; j++)
+		s[j] = str[j];
+	s[j] = '\0';
+	return (s);
+}
