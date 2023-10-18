eb5a79cc88f19ea271fb5d146355246e669682b6
oumaima <oumaimanaanaapro@gmail.com>
2023-10-18 11:24:23 +0100

    add customized functions in function_3.c

--- function_3.c
+++ function_3.c
@@ -3,6 +3,8 @@
  * print_number - prints an integers
  *
  * @n: the integer to be printed
+ *
+ * @std: standard
  */
 void print_number(int std, int n)
 {
@@ -29,6 +31,8 @@ void print_number(int std, int n)
  * @line: number of the line in sh
  *
  * @command: command name
+ *
+ * @prefix: message to print
  */
 
 void custom_perror(char *program_name, int line, char *prefix, char *command)
@@ -46,4 +50,93 @@ void custom_perror(char *program_name, int line, char *prefix, char *command)
 	write(STDERR_FILENO, prefix, _strlen(prefix));
 	write(STDERR_FILENO, new_line, _strlen(new_line));
 }
+/**
+ * custom_perror_exit - prints message error for exit string and negnumber
+ *
+ * @program_name: name of executable file
+ *
+ * @line: index of line in terminal
+ *
+ * @prefix: the message to print
+ *
+ * @command: argument given after exit
+ */
+void custom_perror_exit(char *program_name, int line, char
+		*prefix, char *command)
+{
+
+	char *suffix = ": ";
+	char *new_line = "\n";
+
+
+	write(STDERR_FILENO, program_name, _strlen(program_name));
+	write(STDERR_FILENO, suffix, _strlen(suffix));
+	print_number(2, line);
+	write(STDERR_FILENO, suffix, _strlen(suffix));
+	write(STDERR_FILENO, prefix, _strlen(prefix));
+	write(STDERR_FILENO, command, _strlen(command));
+	write(STDERR_FILENO, new_line, _strlen(new_line));
+}
+/**
+ * _isalpha_string - checks a whole string with _isalpha
+ *
+ * @arg: the given string to checks
+ *
+ * Return: 1 is a letter is found or 0 otherwise
+ */
+int _isalpha_string(char *arg)
+{
+	int i, alpha, r;
+
+	for (i = 0; arg[i]; i++)
+	{
+		alpha = _isalpha(arg[i]);
+	}
+	if (alpha == 1)
+	{
+		r = 1;
+	}
+	else if (alpha == 0)
+	{
+		r = 0;
+	}
+	return (r);
+}
+
+/**
+ * print_env - prints a 2D array of char
+ *
+ * @env: the 2D array of char to print
+ *
+ */
+void print_env(char **env)
+{
+	int env_size = 0, i;
+	char **env_cpy = NULL;
+
+	for (i = 0; env[i]; i++)
+	{
+		env_size += _strlen(env[i]) + 1;
+	}
+	env_cpy = malloc((env_size + 1) * sizeof(char));
+
+	for (i = 0; env[i]; i++)
+	{
+		env_cpy[i] = (char *)malloc(_strlen(env[i]) + 1 * sizeof(char));
+		_strcpy(env_cpy[i], env[i]);
+	}
+	env_cpy[i] = NULL;
+
+	for (i = 0; env_cpy[i]; i++)
+	{
+		_puts(1, env_cpy[i]);
+	}
+	/*_putchar(1, '\n');*/
+	for (i = 0; env_cpy[i]; i++)
+	{
+		free(env_cpy[i]);
+	}
+	free(env_cpy);
+}
+
 
